d3609df600329ee58bb226fa02027682681a5b62
jonathan akuffo <jonathan.akuffo@gmail.com>
2020-11-05 08:28:54 +0000

    update

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const exit = document.querySelector(".exit");
+const layer = document.querySelector("#layer");
+const box = document.querySelectorAll(".box")
+
+exit.addEventListener("click", () =>{
+
+    layer.classList.remove("active")
+})
